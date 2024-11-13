a3bcd0611af3cb3b8eed1f9b7058331dcb9847be
Dim-crea <hauterville.dimitry@gmail.com>
2024-11-13 22:58:40 +0100

    New version

--- app/Acceuil/ProgramComponent.js
+++ app/Acceuil/ProgramComponent.js
@@ -11,6 +11,8 @@ import Zumba from "@/app/Assets/RAW02463 1.png"
 import Yoga from "@/app/Assets/Yoga.png"
 import Brunch from "@/app/Assets/Brunch.png"
 import EmojiDanse from "@/app/Assets/danse.png"
+import EmojiBrunch from "@/app/Assets/EmojiBrunch.png"
+import EmojiYoga from "@/app/Assets/EmojiYoga.png"
 import { TriangleAlert } from "lucide-react";
 
 export default function ProgramComponent() {
@@ -20,7 +22,7 @@ export default function ProgramComponent() {
             <Carousel className="mt-4 ">
                 <CarouselContent>
                     <CarouselItem>
-                        <div className="rounded-3xl bg-fondCard bg-opacity-60 p-4">
+                        <div className="rounded-3xl bg-fondCard bg-opacity-60 p-4 flex flex-col">
                             <h2 className="font-semibold text-center py-2 flex justify-center">Zumba pour se défouler <Image src={EmojiDanse}/></h2>
                             <Image 
                             src={Zumba}
@@ -38,27 +40,27 @@ export default function ProgramComponent() {
                     </CarouselItem>
 
                     <CarouselItem>
-                        <div className="rounded-3xl  bg-fondCard bg-opacity-60 px-6">
-                            <h2 className="font-semibold text-center py-2 flex justify-center">Yoga pour se ressourcer <Image src={EmojiDanse}/></h2>
+                        <div className="rounded-3xl  bg-fondCard bg-opacity-60 px-6 flex flex-col">
+                            <h2 className="font-semibold text-center py-2 flex justify-center">Brunch pour se régaler <Image src={EmojiBrunch}/></h2>
                             <Image 
                             src={Brunch}
                             className="place-self-center"
                             />
                             <p className="text-center my-4 mb-6">
-                                Recharge tes batteries avec notre brunch, placé entre les cours de Zumba et de Yoga ! Profite d’une pause gourmande et équilibrée pour savourer des plats délicieux, partager un moment convivial et préparer votre corps à une journée de détente totale. Un dimanche sous le signe du plaisir et de la vitalité!
+                                Recharge tes batteries avec notre brunch, placé entre les cours de Zumba et de Yoga ! Profite d’une pause gourmande et équilibrée pour savourer des plats délicieux, partager un moment convivial et préparer ton corps à une journée de détente totale. Un dimanche sous le signe du plaisir et de la vitalité!
                             </p>
                         </div>
                     </CarouselItem>
 
                     <CarouselItem>
-                        <div className="rounded-3xl bg-fondCard bg-opacity-60 p-4">
-                            <h2 className="font-semibold text-center py-2 flex justify-center">Yoga pour se ressourcer <Image src={EmojiDanse}/></h2>
+                        <div className="rounded-3xl bg-fondCard bg-opacity-60 p-4 flex flex-col">
+                            <h2 className="font-semibold text-center py-2 flex justify-center">Yoga pour se ressourcer <Image src={EmojiYoga}/></h2>
                             <Image 
                             src={Yoga}
                             className="place-self-center"
                             />
                             <p className="text-center my-4">
-                                On termine par un moment de reconnexion avec notre cours de yoga ouvert à tous les niveaux ! Que tu sois débutant ou confirmé, profites de postures adaptées afin de relâcher les tensions, gagner en souplesse et retrouver ton énergie. Rejoins-nous pour un instant de calme et de revitalisation !
+                                On termine par un moment de reconnexion avec notre cours de yoga ouvert à tous les niveaux ! Que tu sois débutant ou confirmé, profite de postures adaptées afin de relâcher les tensions, gagner en souplesse et retrouver ton énergie. Rejoins-nous pour un instant de calme et de revitalisation !
                             </p>
                             <p className="text-center my-2">de <span className="font-semibold">10h</span> à <span className="font-semibold">10h45</span></p>
                             <div className="flex justify-center">
--- app/Snippets/BrochureSnip.js
+++ app/Snippets/BrochureSnip.js
@@ -4,7 +4,7 @@ import { Download } from "lucide-react";
 export default function BrochureSnip(){
 
     return(
-        <a href="./Assets/Flyer Sporty Brunch.pdf" download>
+        <a href="@/app/Assets/FlyerSportyBrunchA4.png" download>
         <div className=" text-center py-2 my-4 mx-4 bg-fondCard bg-opacity-70 rounded-xl flex justify-between">
             <p className="text-xl ml-10 text-accentuationColor">Télécharger la brochure</p>
             <Download className="mr-4"/>
