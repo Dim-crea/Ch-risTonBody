7573ddf157f0b4dfd7f8bfbc85a0ea37bf3cc5a3
Dim-crea <hauterville.dimitry@gmail.com>
2026-01-03 17:07:08 +0100

    update

--- app/page.js
+++ app/page.js
@@ -6,14 +6,17 @@ import BrochureSnip from "./Snippets/BrochureSnip";
 export default function Home() {
   return (
     <div>
-      <IntroComponent />
+      {/*<IntroComponent />
       <ProgramComponent />
       <ReservComponent />
-      {/* <BrochureSnip /> */}
-      <div className="flex flex-col text-center my-10 ">
+      <BrochureSnip /> 
+      <footer className="flex flex-col text-center my-10 ">
         <a>Mentions légales</a>
         <a>Politique de cookies</a>
         <a>Politique de confidentialités</a>
+      </footer>*/}
+      <div>
+        <h1 className="flex items-center justify-center h-screen">Site bientôt disponible</h1>
       </div>
     </div>
   );
